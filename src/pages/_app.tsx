23b33bbf64b66aebb9d091ce94ca3fc79605757f
CBWDG <zaratole@naver.com>
2023-12-06 10:51:50 +0900

    feat(#4): global css 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,21 +4,12 @@ import React from 'react';
 import { RecoilRoot } from 'recoil';
 import styled from 'styled-components';
 
-const PageWrapper = styled.div`
-  background: #252525;
-  display: flex;
-  margin: -10px 0px -10px -10px;
-  width: 100vw;
-  height: 100vh;
-`;
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PageWrapper>
-      <RecoilRoot>
-        <React.Suspense fallback={<div>Loading...</div>}>
-          <Component {...pageProps} />
-        </React.Suspense>
-      </RecoilRoot>
-    </PageWrapper>
+    <RecoilRoot>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <Component {...pageProps} />
+      </React.Suspense>
+    </RecoilRoot>
   );
 }
