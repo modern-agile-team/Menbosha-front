7ed4516cb6bb89a25191d7c38c413f3f2cc891dd
CBWDG <zaratole@naver.com>
2023-12-07 17:57:42 +0900

    feat(#11): body작업 중

--- src/components/organisms/mainpagebody/styled.ts
+++ src/components/organisms/mainpagebody/styled.ts
@@ -2,24 +2,20 @@ import styled from 'styled-components';
 
 export const BodyWrapper = styled.div`
   display: flex;
-  width: inherit;
+  width: 100vw;
   height: 70vh;
-  border: 2px solid white;
+  /* border: 2px solid white; */
 `;
 
 export const BodyContainer = styled.div`
+  background-image: url('/MainBody-example.png');
+  background-size: cover;
   display: flex;
-  margin-left: 197.5px;
-  width: 88vw;
+  margin: 0px 197.5px;
+  width: 76.5vw;
   height: inherit;
   align-items: center;
   justify-content: space-between;
-  border: 2px solid yellow;
-`;
-
-export const BodyBox = styled.div`
-  display: flex;
-  width: inherit;
-  height: 60vh;
-  border: 2px solid green;
+  border-radius: 30px;
+  /* border: 2px solid yellow; */
 `;
