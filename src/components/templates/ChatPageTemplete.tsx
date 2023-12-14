95a5ef059363bc21deca77e3fdc2c8acba905483
CBWDG <zaratole@naver.com>
2023-12-14 19:19:57 +0900

    feat(#18): commit 한번 정도는 괜찮잖아

--- src/components/templates/ChatPageTemplete.tsx
+++ src/components/templates/ChatPageTemplete.tsx
@@ -4,7 +4,10 @@ import { PageWrapper } from './styled';
 const ChatPageTemplete = () => {
   return (
     <PageWrapper>
-      <span style={{ fontSize: '100px', color: 'white' }}>채팅 페이지</span>;
+      <span style={{ fontSize: '100px', color: 'white' }}>
+        채팅 페이지 커밍 쑨
+      </span>
+      ;
     </PageWrapper>
   );
 };
