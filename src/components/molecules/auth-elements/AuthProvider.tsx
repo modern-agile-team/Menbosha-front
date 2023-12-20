b9c36966154b320d1e43be7b0d05ef687804be1d
zzzRYT <jinjinstar3@gmail.com>
2023-12-20 12:31:44 +0900

    feat(#8):수정1

--- src/components/molecules/auth-elements/AuthProvider.tsx
+++ src/components/molecules/auth-elements/AuthProvider.tsx
@@ -11,12 +11,11 @@ const getRandomInt = (min: number, max: number) => {
 };
 export const Naver = () => {
   const naverLogin = () => {
-    // window.location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${
-    //   process.env.NEXT_PUBLIC_NAVER_CLIENT_ID
-    // }&redirect_uri=${
-    //   process.env.NEXT_PUBLIC_NAVER_REDIRECT_URL
-    // }&state=${getRandomInt(1, 9999)}`;.
-    window.location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_NAVER_REDIRECT_URL}&state=test`;
+    window.location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${
+      process.env.NEXT_PUBLIC_NAVER_CLIENT_ID
+    }&redirect_uri=${
+      process.env.NEXT_PUBLIC_NAVER_REDIRECT_URL
+    }&state=${getRandomInt(1, 9999)}`;
   };
 
   return (
@@ -34,12 +33,11 @@ export const Naver = () => {
 
 export const Kakao = () => {
   const kakaoLogin = () => {
-    // window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${
-    //   process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID
-    // }&redirect_uri=${
-    //   process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URL
-    // }&response_type=code&state=${getRandomInt(1, 9999)}`;
-    window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URL}&response_type=code&state=test`;
+    window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${
+      process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID
+    }&redirect_uri=${
+      process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URL
+    }&response_type=code&state=${getRandomInt(1, 9999)}`;
   };
   return (
     <div>
@@ -56,13 +54,12 @@ export const Kakao = () => {
 
 export const Google = () => {
   const googleLogin = () => {
-    // window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A//www.googleapis.com/auth/drive.metadata.readonly&access_type=offline&include_granted_scopes=true&response_type=code&state=${getRandomInt(
-    //   1,
-    //   9999,
-    // )}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URL}&client_id=${
-    //   process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID
-    // }`;
-    window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A//www.googleapis.com/auth/drive.metadata.readonly&access_type=offline&include_granted_scopes=true&response_type=code&state=test&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URL}&client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}`;
+    window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A//www.googleapis.com/auth/drive.metadata.readonly&access_type=offline&include_granted_scopes=true&response_type=code&state=${getRandomInt(
+      1,
+      9999,
+    )}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URL}&client_id=${
+      process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID
+    }`;
   };
   return (
     <div>
