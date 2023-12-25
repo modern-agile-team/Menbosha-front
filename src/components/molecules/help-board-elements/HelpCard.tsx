a404dcf8ddb5bf910bf06c2a0b50bd93210f73d7
zzzRYT <jinjinstar3@gmail.com>
2023-12-25 22:45:17 +0900

    feat(#36): 도와주세요 보드 unit 연결

--- src/components/molecules/help-board-elements/HelpCard.tsx
+++ src/components/molecules/help-board-elements/HelpCard.tsx
@@ -1,5 +1,6 @@
 import { TextBox } from '@/components/common/globalStyled/styled';
 import * as S from './styled';
+import Link from 'next/link';
 
 interface CardType {
   id: number;
@@ -14,16 +15,27 @@ interface CardType {
 const HelpCard = (props: CardType) => {
   return (
     <S.CardContainer>
-      <S.CardImageBox>이미지이미지이미지</S.CardImageBox>
-      <TextBox size={20} color="#C63D2F" style={{ padding: '13px 0px' }}>
-        {props.head}
-      </TextBox>
-      <TextBox size={14} color="#fff" style={{ padding: '0px 0px 10px 0px' }}>
-        {props.body}
-      </TextBox>
-      <TextBox size={14} color="#fff" style={{ padding: '10px 0px 10px 0px' }}>
-        {props.createAt}
-      </TextBox>
+      <Link
+        href={{
+          pathname: `/help/unit`,
+          query: {
+            id: props.id,
+          },
+        }}>
+        <S.CardImageBox>이미지이미지이미지</S.CardImageBox>
+        <TextBox size={20} color="#C63D2F" style={{ padding: '13px 0px' }}>
+          {props.head}
+        </TextBox>
+        <TextBox size={14} color="#fff" style={{ padding: '0px 0px 10px 0px' }}>
+          {props.body}
+        </TextBox>
+        <TextBox
+          size={14}
+          color="#fff"
+          style={{ padding: '10px 0px 10px 0px' }}>
+          {props.createAt}
+        </TextBox>
+      </Link>
     </S.CardContainer>
   );
 };
