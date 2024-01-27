b7fb880748d7de73cdc583ada3dc6bc3bfe69a0b
CBWDG <zaratole@naver.com>
2024-01-27 15:49:09 +0900

    publish(#112): mainContents2-3

--- src/components/organisms/mainpage-body/MainPageBody.tsx
+++ src/components/organisms/mainpage-body/MainPageBody.tsx
@@ -17,7 +17,15 @@ const MainPageBody = () => {
         <S.MainContentsContainer2>
           <S.MainContents2Left src="https://menbosha-s3.s3.ap-northeast-2.amazonaws.com/public/mainpage/mainLeaf.svg" />
           <S.MainContents2Right>
-            <span>바로 해결하는 멘토링.</span>
+            <span
+              style={{
+                fontSize: '4em',
+                fontWeight: '700',
+                marginBottom: '2vh',
+              }}>
+              {/* 아니 이거왜그런지 모르겠는데 이 부분 span태그만 css코드가 안먹네요;; */}
+              바로 해결하는 멘토링.
+            </span>
             <div>
               멘보샤는 질문 - 채팅 - 해결이라는 직관적이고 빠른 시스템으로
               문제를 해결하는데 소요되는 시간을 많이 단축시킬 수 있습니다. 마감
