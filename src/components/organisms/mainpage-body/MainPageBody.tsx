b1929907f4e5767ca769643649c7a3b85325a6a8
CBWDG <zaratole@naver.com>
2024-01-27 12:28:44 +0900

    publish(#112): mainContents2

--- src/components/organisms/mainpage-body/MainPageBody.tsx
+++ src/components/organisms/mainpage-body/MainPageBody.tsx
@@ -18,19 +18,40 @@ const MainPageBody = () => {
           <S.MainContents2Left src="https://menbosha-s3.s3.ap-northeast-2.amazonaws.com/public/mainpage/mainLeaf.svg" />
           <S.MainContents2Right>
             <span>바로 해결하는 멘토링.</span>
-            <span>
+            <div>
               멘보샤는 질문 - 채팅 - 해결이라는 직관적이고 빠른 시스템으로
               문제를 해결하는데 소요되는 시간을 많이 단축시킬 수 있습니다. 마감
               시간이 다가올 때, 내일 당장 무언가를 해야할 때 멘보샤를 통해
               빠르게 답변을 받아보세요.
-            </span>
-            <S.MainContents2RightBottom>
-              <span>질문 작성</span>
-              <span>멘토 확인 및 1대1 채팅</span>
-              <span>해결</span>
-            </S.MainContents2RightBottom>
+            </div>
+            <S.MainContents2RightBottom />
           </S.MainContents2Right>
         </S.MainContentsContainer2>
+        <S.MainContentsContainer3>
+          <S.MainContents3Left src="https://menbosha-s3.s3.ap-northeast-2.amazonaws.com/public/mainpage/mainLeaf.svg" />
+          <S.MainContents3Right>
+            <span>가볍지만 체계적으로.</span>
+            <div>
+              단순하고 가벼운 시스템에서도 사소한 사고가 발생하지 않도록
+              체계적인 시스템을 구축했습니다. 나쁜 경험이 없도록 최선을 다하는
+              멘보샤입니다.
+            </div>
+            <S.MainContents3InfoContainer>
+              <S.InfoBox>
+                <span>원활한 의사소통을 위한 선택적 채팅 시스템</span>
+              </S.InfoBox>
+              <S.InfoBox>
+                멘토가 마음에 들었다면? 채팅 종료 후 리뷰 진행
+              </S.InfoBox>
+              <S.InfoBox>
+                멘토로서 신뢰도를 얻을 수 있는 온도 표시 시스템
+              </S.InfoBox>
+              <S.InfoBox>
+                알려주기만 하면 뭐해? 최고의 멘토에 대한 보상
+              </S.InfoBox>
+            </S.MainContents3InfoContainer>
+          </S.MainContents3Right>
+        </S.MainContentsContainer3>
       </S.BodyContainer>
     </S.BodyWrapper>
   );
--- src/components/organisms/mainpage-body/styled.tsx
+++ src/components/organisms/mainpage-body/styled.tsx
@@ -17,13 +17,25 @@ export const BodyContainer = styled.div`
 
 export const MainContentsContainer1 = styled.div`
   display: flex;
-  background-image: url('https://menbosha-s3.s3.ap-northeast-2.amazonaws.com/public/mainpage/mainMenbosha.svg');
+  background: linear-gradient(
+      to bottom,
+      rgba(255, 255, 255, 0) 10%,
+      rgba(255, 255, 255, 0) 20%,
+      rgba(255, 255, 255, 0) 30%,
+      rgba(255, 255, 255, 0) 40%,
+      rgba(255, 255, 255, 0.2) 50%,
+      rgba(255, 255, 255, 0.7) 75%,
+      rgba(255, 255, 255, 1) 90%,
+      rgba(255, 255, 255, 1) 100%
+    ),
+    url('https://menbosha-s3.s3.ap-northeast-2.amazonaws.com/public/mainpage/mainMenbosha.svg');
+  background-size: cover;
+  background-repeat: no-repeat;
   width: 100vw;
   height: 100vh;
   justify-content: center;
   align-items: center;
-  /* z-index: -999; */
-  /* border: 2px solid red; */
+  border: none;
 `;
 
 export const MenboshaTitleBoxColumn = styled.div`
@@ -47,15 +59,16 @@ export const MainContentsContainer2 = styled.div`
   display: flex;
   width: 80vw;
   height: 100vh;
+  padding-top: 10vh;
   color: #000000;
-  border: 2px solid black;
+  /* border: 2px solid black; */
 `;
 
 export const MainContents2Left = styled.img`
   width: 7vw;
   height: 12vh;
   margin-right: 1vw;
-  border: 2px solid blue;
+  /* border: 2px solid blue; */
 `;
 
 export const MainContents2Right = styled.div`
@@ -64,16 +77,18 @@ export const MainContents2Right = styled.div`
   width: 52vw;
   height: 80vh;
   border: 2px solid orange;
-  :nth-child(1) {
+  / & > span {
     margin-bottom: 2vh;
     font-size: 4em;
     font-weight: 700;
   }
-  :nth-child(2) {
-    margin-bottom: 6vh;
+  & > div {
+    margin-bottom: 5vh;
+    width: 40vw;
     font-size: 0.83em;
     font-weight: 700;
     line-height: 150%;
+    text-align: justify;
   }
   :nth-child(3) {
     align-self: flex-end;
@@ -83,33 +98,63 @@ export const MainContents2Right = styled.div`
 export const MainContents2RightBottom = styled.div`
   display: flex;
   flex-direction: column;
-  background-image: url('https://menbosha-s3.s3.ap-northeast-2.amazonaws.com/public/mainpage/mainContents1.svg');
+  bottom: 0;
+  right: 0;
+  background-image: url('https://menbosha-s3.s3.ap-northeast-2.amazonaws.com/public/mainpage/mainContents.svg');
   background-repeat: no-repeat;
   background-size: contain;
+  background-position: center;
   min-width: 40vw; // 742
   min-height: 50vh; //526
-  border: 2px solid lightgreen;
-  :nth-child(1) {
-    margin: 11vh 0px 0px 10vw;
-    font-size: 1.5em;
+`;
+
+export const MainContentsContainer3 = styled.div`
+  display: flex;
+  width: 80vw;
+  height: 100vh;
+  color: #000000;
+  border: 2px solid black;
+`;
+
+export const MainContents3Left = styled.img`
+  width: 7vw;
+  height: 12vh;
+  margin-right: 1vw;
+  border: 2px solid blue;
+`;
+
+export const MainContents3Right = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 55vw;
+  height: 80vh;
+  border: 2px solid orange;
+  & > span {
+    margin-bottom: 2vh;
+    font-size: 4em;
     font-weight: 700;
-    color: #ffffff;
-    line-height: 120%;
   }
-  :nth-child(2) {
-    text-align: center;
-    margin: 8vh 0px 0px 20vw;
-    max-width: 10vw;
-    font-size: 1.5em;
+  & > div {
+    margin-bottom: 5vh;
+    width: 44vw;
+    font-size: 0.83em;
     font-weight: 700;
-    color: #ffffff;
-    line-height: 120%;
+    line-height: 150%;
+    text-align: justify;
   }
-  :nth-child(3) {
-    margin: 4vh 6vw 0px 0px;
-    font-size: 1.5em;
-    font-weight: 700;
-    color: #ffffff;
-    line-height: 120%;
-  }
+`;
+
+export const MainContents3InfoContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  min-width: 46vw; // 890
+  min-height: 50vh; //541
+  border: 2px solid black;
+`;
+
+export const InfoBox = styled.div`
+  display: flex;
+  border: 2px solid red;
+  text-align: justify;
 `;
