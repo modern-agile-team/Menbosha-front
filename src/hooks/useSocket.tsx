be2016689837212cc2e044a42b9d6cbe33c15958
CBWDG <zaratole@naver.com>
2024-02-23 13:17:24 +0900

    feat(#114): SocketURL

--- src/hooks/useSocket.tsx
+++ src/hooks/useSocket.tsx
@@ -21,7 +21,8 @@ export const SocketProvider = ({ children }: SocketProviderProps) => {
   useEffect(() => {
     //수정 해야함
     // const socket = io('http://54.180.108.185:3000/chat');
-    const socket = io(`${process.env.NEXT_PUBLIC_API_BASE_URL}chat`);
+    // const socket = io(`${process.env.NEXT_PUBLIC_API_BASE_URL}chat`);
+    const socket = io('https://api.menbosha.kr');
     setSocket(socket);
     console.log('socketsocketsocketsocket', socket);
 
