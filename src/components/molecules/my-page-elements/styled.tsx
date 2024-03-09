0ceb71f8b28d834a175a966473b2cc850074c64a
zzzRYT <jinjinstar3@gamil.com>
2024-03-09 17:47:36 +0900

    modify(#172):리뷰 남기기 확인, 에러 수정

--- src/components/molecules/my-page-elements/styled.tsx
+++ src/components/molecules/my-page-elements/styled.tsx
@@ -4,7 +4,7 @@ import styled, { css } from 'styled-components';
 //마이 랭크
 export const MyRankWrapper = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 16.6vw 16.6vw 16.6vw;
   grid-template-rows: 1fr;
   //페이지 타이틀
   & > :nth-child(1) {
@@ -31,11 +31,11 @@ export const MyRankNameAndScore = styled.div`
   color: #fff;
   margin-left: 55px;
   & > :nth-child(1) {
-    font-size: 1.5em; //36px
+    font-size: 1.88vw; //36px
     font-weight: 700; //Pretendard-Bold
   }
   & > :nth-child(2) {
-    font-size: 0.65em; //15.6px
+    font-size: 0.83vw; //15.936px
     font-weight: 400; //Pretendard-Regular
     margin-top: 5px;
   }
@@ -44,22 +44,28 @@ export const MyRankNameAndScore = styled.div`
 //랭크 레벨, 해금 단계 컨테이너
 export const RankLevelLockContainer = styled.div`
   display: grid;
-  margin-top: 50px;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  margin-top: 5vh;
+  grid-template-columns: 10vw 10vw 10vw 10vw 10vw;
   grid-template-rows: 1fr 0fr 1fr;
+  //해금 랭크 이미지들
   & > :nth-child(1),
   :nth-child(2),
   :nth-child(3),
   :nth-child(4),
   :nth-child(5) {
     text-align: center;
+    & > img {
+      width: 5vw;
+      height: 5vw;
+    }
   }
+  //그라데이션 바
   & > :nth-child(6) {
+    width: 50vw;
     grid-column: 1/ 6;
-
     border: 5px solid transparent;
     border-radius: 10px;
-    margin: 23px 0px;
+    margin: 2.1vh 0px;
     background-image: linear-gradient(#fff, #fff),
       linear-gradient(
         to right,
@@ -72,6 +78,7 @@ export const RankLevelLockContainer = styled.div`
     background-origin: border-box;
     background-clip: content-box, border-box;
   }
+  //해금 랭크 이름
   & > :nth-child(7),
   :nth-child(8),
   :nth-child(9),
@@ -80,12 +87,12 @@ export const RankLevelLockContainer = styled.div`
     text-align: center;
 
     & > :nth-child(1) {
-      font-size: 1em; //24px
+      font-size: 1.25vw; //24px
       font-weight: 700; //Pretendard-Bold
       color: #fff;
     }
     & > :nth-child(2) {
-      font-size: 0.67em; //16.8px
+      font-size: 0.83vw; //15.936px
       font-weight: 400; //Pretendard-Regular
       margin-top: 5px;
       color: #fff;
@@ -96,7 +103,7 @@ export const RankLevelLockContainer = styled.div`
 //내 칭호
 export const BadgeListWrapper = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  grid-template-columns: 10vw 10vw 10vw 10vw 10vw;
   place-items: center;
   color: #fff;
 `;
@@ -110,26 +117,37 @@ export const PageCountContainer = styled.div`
   }
 `;
 
+export const UnlockBadgeBox = styled.div`
+  & > :nth-child(1) {
+    width: 10vw;
+    height: 9.5vh;
+    margin-top: 5px;
+  }
+`;
+
 export const UnlockBadgePreviewBox = styled.div`
   & > :nth-child(1) {
     display: block;
+    width: 10vw;
+    height: 11vh;
   }
   & > :nth-child(2) {
     display: none;
+    width: 10vw;
+    height: 11vh;
   }
   &:hover {
     & > :nth-child(1) {
       display: none;
+      width: 8vw;
+      height: 11vh;
     }
     & > :nth-child(2) {
       display: block;
+      width: 8vw;
+      height: 11vh;
     }
   }
-`;
-
-export const BadgeImageBox = styled.img`
-  width: 167px;
-  height: 99px;
 `;
 
 interface BtnType {
