5bf808c1a77bf207641efb88294767c9fa56ce03
CBWDG <zaratole@naver.com>
2024-03-18 13:22:09 +0900

    fix(#199): 여러 에러핸들링

--- src/components/molecules/chat-space-elements/chat-space-header/ChatSpaceHeader.tsx
+++ src/components/molecules/chat-space-elements/chat-space-header/ChatSpaceHeader.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styled';
 import Image from 'next/image';
 import { ChatPartnersType } from '@/types/chat';
@@ -6,15 +6,21 @@ import useModal from '@/hooks/useModal';
 import ReviewModal from '@/components/organisms/review/ReviewModal';
 import { useRecoilValue } from 'recoil';
 import { SelectedRoomIdAtom } from '@/recoil/atoms/SelectedRoomIdAtom';
+import ChatInfoModal from './ChatInfoModal';
 
 const ChatSpaceHeader = (props: {
   chatPartners: ChatPartnersType | undefined;
 }) => {
   const { chatPartners } = props;
   const { isOpenModal, handleModal } = useModal();
+  const [isOpenInfo, setIsOpenInfo] = useState(false);
   const selectedRoomId = useRecoilValue(SelectedRoomIdAtom);
 
   const isRoomSelected = selectedRoomId !== '';
+
+  const handleInfoModal = () => {
+    setIsOpenInfo(!isOpenInfo);
+  };
 
   if (!isRoomSelected) {
     return (
@@ -23,7 +29,7 @@ const ChatSpaceHeader = (props: {
           <S.ChatSpaceHeaderLeft>
             <S.ChatSpaceHeaderGuestImage
               src={
-                'https://menbosha-s3.s3.ap-northeast-2.amazonaws.com/public/chat/UserImage-white.svg'
+                'https://menbosha-s3.s3.ap-northeast-2.amazonaws.com/public/chat/userImage-latest.svg'
               }
               alt="GuestIcon"
             />
@@ -34,19 +40,29 @@ const ChatSpaceHeader = (props: {
               alt="Info"
               width="28"
               height="28"
+              onClick={handleInfoModal}
             />
             <Image
               src="https://menbosha-s3.s3.ap-northeast-2.amazonaws.com/public/chat/ChatReport.svg"
-              alt="Info"
+              alt="Report"
               width="28"
               height="28"
+              onClick={() => {
+                alert('신고하기 기능은 아직 구현되지 않았습니다.');
+              }}
             />
             <Image
               src="https://menbosha-s3.s3.ap-northeast-2.amazonaws.com/public/chat/ChatRoomOut.svg"
-              alt="Info"
+              alt="Review"
               width="28"
               height="28"
+              onClick={() => {
+                alert('채팅방을 선택해 주세요.');
+              }}
             />
+            {isOpenInfo && (
+              <ChatInfoModal show={isOpenInfo} hide={handleInfoModal} />
+            )}
           </S.ChatSpaceHeaderRight>
         </S.ChatSpaceHeaderArea>
       </S.ChatSpaceHeaderContainer>
@@ -60,7 +76,7 @@ const ChatSpaceHeader = (props: {
           <S.ChatSpaceHeaderGuestImage
             src={
               chatPartners?.userImage ||
-              'https://menbosha-s3.s3.ap-northeast-2.amazonaws.com/public/chat/UserImage-white.svg'
+              'https://menbosha-s3.s3.ap-northeast-2.amazonaws.com/public/chat/userImage-latest.svg'
             }
             alt="GuestIcon"
           />
@@ -72,20 +88,27 @@ const ChatSpaceHeader = (props: {
             alt="Info"
             width="28"
             height="28"
+            onClick={handleInfoModal}
           />
           <Image
             src="https://menbosha-s3.s3.ap-northeast-2.amazonaws.com/public/chat/ChatReport.svg"
-            alt="Info"
+            alt="Report"
             width="28"
             height="28"
+            onClick={() => {
+              alert('신고하기 기능은 아직 구현되지 않았습니다.');
+            }}
           />
           <Image
             onClick={handleModal}
             src="https://menbosha-s3.s3.ap-northeast-2.amazonaws.com/public/chat/ChatRoomOut.svg"
-            alt="Info"
+            alt="Review"
             width="28"
             height="28"
           />
+          {isOpenInfo && (
+            <ChatInfoModal show={isOpenInfo} hide={handleInfoModal} />
+          )}
           {isOpenModal && (
             <ReviewModal
               mentorId={chatPartners?.id as number}
