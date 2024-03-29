6d64d60c712b964fba04d60e4f0a42fb168c0dfb
zzzRYT <jinjinstar3@gmail.com>
2024-03-29 16:02:17 +0900

    feat(#248):사이트 프리뷰 생성 로직 추가

--- src/components/common/Seo.tsx
+++ src/components/common/Seo.tsx
@@ -12,6 +12,19 @@ export default function Seo() {
         name="naver-site-verification"
         content="f70d229d3e38d57d3101592c57fb452c85668743"
       />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://menbosha.kr/" />
+      <meta property="og:title" content="멘보샤처럼 맛있게 우리의 멘토멘티" />
+      <meta property="og:image" content="https://example.com/image.jpg" />
+      <meta
+        property="og:description"
+        content="멘보샤 처럼 맛있는 멘토와 멘티를 진행해 보세요. 누구나 멘토가 될 수 있고, 누구나 멘티가 될 수 있습니다."
+      />
+      <meta property="og:site_name" content="멘보샤" />
+      <meta property="og:locale" content="ko_KR" />
+
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
     </Head>
   );
 }
